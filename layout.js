51da4204ab9cfb4763ea500388f4d0e6cb87dd6a
costack <163998102+CostackLLC@users.noreply.github.com>
2024-04-12 15:28:44 -0400

    Add files via upload

--- layout.js
+++ layout.js
@@ -3,32 +3,40 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const buttons = document.querySelectorAll('.article-topbar-navigation-button, .article-topbar-navigation-close-button');
     const animationDuration = 150; // Duration of the animation in milliseconds
+    const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints; // Check for touch device
 
     buttons.forEach(button => {
-        button.addEventListener('mousedown', function () {
-            this.classList.add('active');
+        if (!isTouchDevice) {
+            // Desktop events
+            button.addEventListener('mousedown', function () {
+                this.classList.add('active');
+            });
+
+            button.addEventListener('mouseup', function () {
+                setTimeout(() => {
+                    this.classList.remove('active');
+                }, animationDuration);
+            });
+
+            button.addEventListener('mouseleave', function () {
+                setTimeout(() => {
+                    this.classList.remove('active');
+                }, animationDuration);
+            });
+        }
+
+        // Touch events
+        button.addEventListener('touchstart', function (e) {
+            // Prevent mouse events from firing
+            e.preventDefault();
+            this.classList.add('hover');
         });
 
-        button.addEventListener('mouseup', function () {
+        button.addEventListener('touchend', function (e) {
+            // Prevent mouse events from firing
+            e.preventDefault();
             setTimeout(() => {
-                this.classList.remove('active');
-            }, animationDuration);
-        });
-
-        button.addEventListener('mouseleave', function () {
-            setTimeout(() => {
-                this.classList.remove('active');
-            }, animationDuration);
-        });
-
-        // For touch devices
-        button.addEventListener('touchstart', function () {
-            this.classList.add('active');
-        });
-
-        button.addEventListener('touchend', function () {
-            setTimeout(() => {
-                this.classList.remove('active');
+                this.classList.remove('hover');
             }, animationDuration);
         });
     });
